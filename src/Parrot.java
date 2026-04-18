670eec68ac0f0ef396bc03ed846c0cc7391a0deb
Milten <dmitry.a.kupriyanov@gmail.com>
2026-04-18 13:29:41 +0300

    Move and sounds done

--- src/Parrot.java
+++ src/Parrot.java
@@ -1,2 +1,26 @@
-public class Parrot {
+import java.util.Random;
+
+public class Parrot extends Bird {
+
+    public Parrot(String featherColoring) {
+        super(featherColoring);
+    }
+
+    @Override
+    public void makeSound() {
+        Random random = new Random();
+        int n = random.nextInt(5);
+        String[] arr_sound = {"Тысяча чертей", "Штиль, ветер молчит", "Чик чирик", "Упал белой чайкой на дно", "Карамба капитан"};
+        System.out.println(arr_sound[n]);
+
+    }
+
+    @Override
+    public void move() {
+        fly();
+    }
+
+    public void fly(){
+        System.out.println("Он улетел, но обещал вернуться");
+    }
 }

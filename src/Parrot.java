e27016286e3761eb2ebedc1579450c6fd93cb071
alkaz07 <alkaz07@yandex.ru>
2026-04-18 13:38:34 +0300

    Parrot corrected

--- src/Parrot.java
+++ src/Parrot.java
@@ -1,18 +1,30 @@
 import java.util.Random;
 
 public class Parrot extends Bird {
+    //память фраз - то свойство каждого отдельного объекта Попугая
+    String[] arr_sound = {"Тысяча чертей", "Штиль, ветер молчит", "Чик чирик", "Упал белой чайкой на дно", "Карамба капитан"};
+    String name="";
 
     public Parrot(String featherColoring) {
         super(featherColoring);
     }
 
+    public Parrot(String featherColoring, String[] sounds) {
+        super(featherColoring);
+        arr_sound = sounds;
+    }
+
+    public Parrot( String name, String featherColoring, String[] arr_sound) {
+        super(featherColoring);
+        this.arr_sound = arr_sound;
+        this.name = name;
+    }
+
     @Override
     public void makeSound() {
         Random random = new Random();
-        int n = random.nextInt(5);
-        String[] arr_sound = {"Тысяча чертей", "Штиль, ветер молчит", "Чик чирик", "Упал белой чайкой на дно", "Карамба капитан"};
+        int n = random.nextInt(arr_sound.length);
         System.out.println(arr_sound[n]);
-
     }
 
     @Override
@@ -21,6 +33,6 @@ public class Parrot extends Bird {
     }
 
     public void fly(){
-        System.out.println("Он улетел, но обещал вернуться");
+        System.out.println(name+" улетел, но обещал вернуться");
     }
 }
